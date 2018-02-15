92c3394ed172754944108d46044add3465ecbc82
aamoutaoufik <aa.moutaoufik@gmail.com>
2018-02-15 15:11:31 -0500

    interface modified

--- resources.py
+++ resources.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: mer. 14. févr. 15:02:48 2018
+# Created: jeu. 15. févr. 13:06:50 2018
 #      by: The Resource Compiler for PyQt (Qt v4.8.5)
 #
 # WARNING! All changes made in this file will be lost!
